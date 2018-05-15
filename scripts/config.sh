821c793329290f3d383d721f63d3ee8876a69a13
Nader Toukabri <nader@emiketic.com>
2018-05-15 01:33:25 +0100

    cleanup

--- scripts/config.sh
+++ scripts/config.sh
@@ -4,6 +4,6 @@ APP_NAME=HelloWorld
 APPETIZE_ANDROID_ID=3xvgukkq4gqjyjn1ztrzq6czwr
 APPETIZE_IOS_ID=nkn34mhpchnx172e67ptmjypdm
 
-DELIVERY_URL=ftp://ftp.astalavistababy.agency/delivery/$(echo $APP_NAME | sed 's/\(.*\)/\L\1/')
+DELIVERY_URL=ftp://ftp.astalavistababy.agency/delivery/$(echo $APP_NAME | sed 's/.*/\L&/')
 
 PROJECT_DIR=$(pwd)
