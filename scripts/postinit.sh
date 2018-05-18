77019be8a33da4cf43e2694e48ded19f02228467
Nader Toukabri <nader@emiketic.com>
2018-05-18 21:54:37 +0100

    cleanup

--- scripts/postinit.sh
+++ scripts/postinit.sh
@@ -3,7 +3,7 @@
 source $(dirname $0)/config.sh
 
 if [ -z "$1" ] || [ -z "$2" ] || [ -z "$3" ] ; then
-  echo "usage: $(basename $0) APP_NAME APP_DISPLAY_NAME APP_STORE_ID" >&2
+  echo "usage: $(basename $0) <APP_NAME> <APP_DISPLAY_NAME> <APP_STORE_ID>" >&2
   exit 1
 fi
 
@@ -14,31 +14,41 @@ APP_STORE_ID=$3
 
 # replace default values
 
-for f in $(find ./ ! -path '*/node_modules/*' ! -path '*/.git/*') ; do
+for f in $(find ./ -type f ! -path '*/node_modules/*' ! -path '*/.git/*') ; do
   sed -i'' "s/HelloWorld/$APP_NAME/" $f
   sed -i'' "s/helloworld/$APP_CODE_NAME/" $f
-  sed -i'' "s/Hello World/$APP_DISPLAY_NAME/" $f
-  sed -i'' "s/Hello App Display Name/$APP_DISPLAY_NAME/" $f
+  sed -i'' "s/Hello\ World/$APP_DISPLAY_NAME/" $f
+  sed -i'' "s/Hello\ App\ Display\ Name/$APP_DISPLAY_NAME/" $f
   sed -i'' "s/com\.$APP_CODE_NAME\.app/$APP_STORE_ID/" $f
 done
 
-cp ./README.md
+#
+
+rm ./README.md
 mv ./README.md.template ./README.md
 
-cp ./package.json
+rm ./package.json
 mv ./package.json.template ./package.json
 
-cp ./scripts/config.sh
+rm ./scripts/config.sh
 mv ./scripts/config.sh.template ./scripts/config.sh
 
-rm ./CLONE.md
+# cleanup
 
-rm ./src/App.js
+[ -d ./.git ] && rm -fr ./.git
+
+rm ./App.js
 
 rm ./scripts/dev*.sh
-rm ./scripts/postinit*.sh
+rm ./scripts/postinit.sh
 
 chmod u+x ./scripts/*.sh
+
+# install dependencies
+
+yarn
+
+#
 
 echo 'Done'
 
