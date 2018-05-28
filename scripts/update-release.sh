1149bd28edcb9efefd634b0183f5bd44c89e15b7
Nader Toukabri <nader@emiketic.com>
2018-05-28 18:58:27 +0100

    fix script

--- scripts/update-release.sh
+++ scripts/update-release.sh
@@ -6,4 +6,4 @@ set -xeo pipefail
 echo "export const RELEASE_VERSION = '$(git describe --always)';" >> ./src/common/release.js
 echo "export const RELEASE_DATE = '$(git show -s --format=%aI)';" >> ./src/common/release.js
 
-# echo "console.ignoredYellowBox = ['Warning:'];" >> ./index.js
+echo "console.ignoredYellowBox = ['Warning:'];" >> ./index.js
