6c89ab217bde408e390bd3ab5efdbc5bd16b63a9
Nader Toukabri <nader@emiketic.com>
2018-05-28 10:26:08 +0100

    cleanup

--- android/app/src/main/java/com/helloworld/MainActivity.java
+++ android/app/src/main/java/com/helloworld/MainActivity.java
@@ -5,6 +5,7 @@ import com.facebook.react.ReactActivity;
 import org.devio.rn.splashscreen.SplashScreen;
 
 public class MainActivity extends ReactActivity {
+
     /**
      * Returns the name of the main component registered from JavaScript.
      * This is used to schedule rendering of the component.
@@ -19,4 +20,5 @@ public class MainActivity extends ReactActivity {
         SplashScreen.show(this);
         super.onCreate(savedInstanceState);
     }
+
 }
