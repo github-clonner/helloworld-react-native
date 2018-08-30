9f77bb138a251300f8d01c340cba9632b61b3c07
Nader Toukabri <nader@emiketic.com>
2018-08-30 19:31:25 +0100

    fix ios

--- ios/HelloWorld/AppDelegate.m
+++ ios/HelloWorld/AppDelegate.m
@@ -10,7 +10,7 @@
 #import <React/RCTBundleURLProvider.h>
 #import <React/RCTRootView.h>
 
-#import "SplashScreen.h"
+#import "RNSplashScreen.h"
 
 
 @implementation AppDelegate
@@ -37,7 +37,7 @@
   self.window.rootViewController = rootViewController;
   [self.window makeKeyAndVisible];
 
-  [SplashScreen show];
+  [RNSplashScreen show];
 
   return YES;
 }
