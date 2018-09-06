d5ccb78488787971616a6895e32688fd21a009bd
Nader Toukabri <nader@emiketic.com>
2018-09-06 08:57:46 +0100

    cleanup assets

--- scripts/postinstall.sh
+++ scripts/postinstall.sh
@@ -1,9 +1,13 @@
 #!/bin/bash -x
 
 # keep only used fonts from native-base
-find ./node_modules/native-base/Fonts/*.ttf  \
-  -not -name 'Ionicons.ttf' \
-  -not -name 'Roboto*.ttf' \
-  -delete
+
+cp ./node_modules/native-base/Fonts/Ionicons.ttf ./assets/fonts/
+cp ./node_modules/native-base/Fonts/Roboto.ttf ./assets/fonts/
+cp ./node_modules/native-base/Fonts/Roboto_medium.ttf ./assets/fonts/
+
+rm -fr ./node_modules/native-base/Fonts/*.ttf || true
+
+# fix Babel issue
 
 rm ./node_modules/emiketic-starter-lib/.babelrc || true
