22139c5e15d10e6821eb3ec897cdb180af515777
Nader Toukabri <nader@emiketic.com>
2018-09-13 20:53:04 +0100

    fix script

--- scripts/generate-all-image-assets-docker.sh
+++ scripts/generate-all-image-assets-docker.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-docker pull emiketic/docker-image-processing
+docker pull emiketic/image-processing
 
-docker run --rm -v $PWD:$PWD -u $(id -u):$(id -g) emiketic/docker-image-processing bash -c  "cd $PWD ; ./scripts/generate-all-image-assets.sh"
+docker run --rm -v $PWD:$PWD -u $(id -u):$(id -g) emiketic/image-processing bash -c  "cd $PWD ; ./scripts/generate-all-image-assets.sh"
