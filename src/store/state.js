b407ab83d81b24abe4ea22cc1c8d9b12616c9581
Nader Toukabri <nader@emiketic.com>
2018-11-02 23:45:41 +0100

    cleanup

--- src/store/state.js
+++ src/store/state.js
@@ -1,14 +1,14 @@
 import * as Activity from '../Shared/Activity.state';
+import * as Shared from '../Shared/state';
 import * as Auth from '../Auth/state';
 import * as Session from '../Session/state';
-import * as Shared from '../Shared/state';
 import * as Home from '../Home/state';
 
 const $state = {
   Activity,
+  Shared,
   Auth,
   Session,
-  Shared,
   Home,
 };
 
