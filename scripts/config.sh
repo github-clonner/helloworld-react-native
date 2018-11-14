1406277d99a57d0a84b0f53c450b1cac1fde21d2
Nader Toukabri <nader@emiketic.com>
2018-11-14 13:07:29 +0100

    enhance ci

--- scripts/config.sh
+++ scripts/config.sh
@@ -2,9 +2,3 @@
 PROJECT_DIR=$(pwd)
 
 APP_NAME=HelloWorld
-
-FTP_DOMAIN=ftp.emiketic.com
-FTP_PATH=/delivery/${APP_NAME,,}
-
-APPETIZE_ANDROID_ID=3xvgukkq4gqjyjn1ztrzq6czwr
-APPETIZE_IOS_ID=nkn34mhpchnx172e67ptmjypdm
--- scripts/upload.sh
+++ scripts/upload.sh
@@ -5,8 +5,8 @@ source $(dirname $0)/config.sh
 
 TARGET=$1
 
-if [ -z "$TARGET" ] || [ ! -f "$TARGET" ] || [ -z "$FTP_USERNAME" ] || [ -z "$FTP_PASSWORD" ]; then
+if [ -z "$TARGET" ] || [ ! -f "$TARGET" ] || [ -z "$DEPLOY_FTP_DOMAIN" ] || [ -z "$DEPLOY_FTP_PATH" ] || [ -z "$DEPLOY_FTP_USERNAME" ] || [ -z "$DEPLOY_FTP_PASSWORD" ]; then
   exit 1
 fi
 
-curl --fail --ftp-create-dirs -u "${FTP_USERNAME}:${FTP_PASSWORD}" "ftp://${FTP_DOMAIN}${FTP_PATH}/${CI_COMMIT_REF_SLUG}/" -T $TARGET
+curl --fail --ftp-create-dirs -u "${DEPLOY_FTP_USERNAME}:${DEPLOY_FTP_PASSWORD}" "ftp://${DEPLOY_FTP_DOMAIN}${DEPLOY_FTP_PATH}/${CI_COMMIT_REF_SLUG}/" -T $TARGET
