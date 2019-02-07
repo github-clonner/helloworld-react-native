b3e38b015ad65e804c3930f84e74565de752a0a7
Nader Toukabri <nader@emiketic.com>
2019-02-07 13:08:57 +0100

    update activity

--- src/Shared/Activity.service.js
+++ src/Shared/Activity.service.js
@@ -1,8 +1,14 @@
 import { Toast } from 'native-base';
 
+import * as Logger from '../common/logger';
+
 import { getStore } from '../store';
 
 import { $processing, $done } from './Activity.state';
+
+/**
+ * Processing
+ */
 
 export function processing(module = 'App', operation = 'default') {
   getStore().dispatch($processing(`${module}.${operation}`));
@@ -12,51 +18,10 @@ export function done(module = 'App', operation = 'default') {
   getStore().dispatch($done(`${module}.${operation}`));
 }
 
-export function toast(type, title, content = '') {}
-
-export function status(type, label) {}
-
-export function alert(type, title, content) {}
-
-export function confirm(title, content, options = {}) {}
-
-
 /**
- * Message
+ * Notification and Alerts
  */
 
-/*
-const message = StateHelper.createSimpleOperation('message');
-
-const Activity_message = 'Activity_message';
-
-export function $message(message, { notify = false } = {}) {
-  return (dispatch) => {
-    Logger.debug('$message', message);
-
-    return message.action({ operation });
-
-    dispatch({
-      type: Activity_message,
-      message,
-    });
-
-    if (notify) {
-      Toast.show({
-        text: message,
-        position: 'bottom',
-        buttonText: 'Ok',
-      });
-    }
-  };
-}
-*/
-
-/**
- * Notification
- */
-
-/*
 const NOTIFICATION_TYPE = {
   success: 'success',
   failure: 'danger',
@@ -65,45 +30,10 @@ const NOTIFICATION_TYPE = {
   default: '',
 };
 
-const status = StateHelper.createSimpleOperation('status');
-
-const Activity_status = 'Activity_status';
-
-export function $status(type, label) {
+export function toast(type, title) {
   type = NOTIFICATION_TYPE[type] || NOTIFICATION_TYPE.default;
 
-  Logger.debug('$status', type, label);
-
-  Toast.show({
-    type,
-    text: label,
-    position: 'bottom',
-    buttonText: 'Ok',
-  });
-
-  return {
-    type: Activity_status,
-    params: {
-      type,
-      label,
-    },
-  };
-}
-*/
-
-/**
- * Toast
- */
-
-/*
-const toast = StateHelper.createSimpleOperation('toast');
-
-const Activity_toast = 'Activity_toast';
-
-export function $toast(type, title, content = '') {
-  type = NOTIFICATION_TYPE[type] || NOTIFICATION_TYPE.default;
-
-  Logger.debug('$toast', type, title, content);
+  Logger.debug('toast', type, title);
 
   Toast.show({
     type,
@@ -111,90 +41,25 @@ export function $toast(type, title, content = '') {
     position: 'bottom',
     buttonText: 'Ok',
   });
-
-  return {
-    type: Activity_toast,
-    params: {
-      type,
-      title,
-      content,
-    },
-  };
-}
-*/
-
-/**
- * Alerts
- */
-
-/*
-const alert = StateHelper.createSimpleOperation('alert');
-
-const Activity_alert = 'Activity_alert';
-
-export function $alert(type, title, content) {
-  Logger.debug('$alert', title, content);
-
-  // Alert.alert(({
-  //   title,
-  //   content,
-  // });
-
-  return {
-    type: Activity_alert,
-    params: {
-      title,
-      content,
-    },
-  };
 }
 
-const CONFIRM_TYPE = {
-  default: 'primary',
-  critical: 'danger',
-};
+export function status(type, message) {
+  type = NOTIFICATION_TYPE[type] || NOTIFICATION_TYPE.default;
 
-let alertReference;
+  Logger.debug('toast', type, message);
 
-const confirm = StateHelper.createSimpleOperation('confirm');
-
-const Activity_confirm = 'Activity_confirm';
-
-export function $confirm(title, content, options = {}) {
-  options.actionType = CONFIRM_TYPE[options.actionType] || CONFIRM_TYPE.default;
-  options.actionLabel = options.actionLabel || 'Confirm';
-
-  Logger.debug('$confim', title, content, options);
-
-  return (dispatch) => {
-    dispatch({
-      type: Activity_confirm,
-      params: {
-        title,
-        content,
-      },
-    });
-
-    if (alertReference) {
-      alertReference.destroy();
-    }
-
-    return new Promise((resolve, reject) => {
-      // alertReference = Modal.confirm({
-      //   title,
-      //   content,
-      //   okType: options.actionType,
-      //   okText: options.actionLabel,
-      //   onOk() {
-      //     alertReference = null;
-      //     resolve(true);
-      //   },
-      //   onCancel() {
-      //     alertReference = null;
-      //     resolve(false);
-      //   },
-      // });
-    });
-  };
+  Toast.show({
+    type,
+    text: message,
+    position: 'bottom',
+    buttonText: 'Ok',
+  });
 }
-*/
+
+export function alert(type, title, content) {
+  // @TODO
+}
+
+export function confirm(title, content, options = {}) {
+  // @TODO
+}
