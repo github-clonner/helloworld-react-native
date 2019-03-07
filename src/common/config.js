8832357d620a113c80115ac4faedcfbc2afc3f9d
Mustapha Turki <stoufa.turki@gmail.com>
2019-03-07 14:07:28 +0100

    Change api endpoint + stop dev

--- src/common/config.js
+++ src/common/config.js
@@ -2,4 +2,4 @@ export { RELEASE_VERSION } from './release';
 
 // export const API_ENDPOINT = 'http://192.168.1.1:5000';
 // export const API_ENDPOINT = 'http://private-5bf85-starterspecapi.apiary-mock.com';
-export const API_ENDPOINT = 'http://192.168.1.13:5000';
+export const API_ENDPOINT = 'http://backend.starter.emiketic.com';
